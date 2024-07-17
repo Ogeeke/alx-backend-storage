10cf064056dae56d0a44e1bd6f396c90c0509e96
ogeeke <ogechieke2014@gmail.com>
2024-07-17 00:14:15 +0000

    all

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''Task 15's module.
+'''
+from pymongo import MongoClient
+
+
+def print_nginx_request_logs(nginx_collection):
+    '''Prints stats about Nginx request logs.
+    '''
+    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        req_count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, req_count))
+    status_checks_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status check'.format(status_checks_count))
+
+
+def print_top_ips(server_collection):
+    '''Prints statistics about the top 10 HTTP IPs in a collection.
+    '''
+    print('IPs:')
